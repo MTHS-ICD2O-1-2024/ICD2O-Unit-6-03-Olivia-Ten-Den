56b6d79a0564a8ddd0ee181ce8bc30e6ed554f6b
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-22 03:01:20 +0000

    updated

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: May 6, 2025
 // This file contains the JS functions for index.html
 
 "use strict"
 
-/**
- * This function gets the Cat fact.
- * The "async" is there because it will take time for the internet to return the value
- */
-async function getCatFact() {
-  // the "try" is here because the internet may not be working
-  // it is like an "if ... else" statement"
+async function getWeather() {
+
   try {
-    const resultJSON = await fetch("https://meowfacts.herokuapp.com/")
-    const jsonData = await resultJSON.json()
-    console.log(jsonData)
-    const randomFactNumber = jsonData.data[0]
+    const weatherJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+    const jsonWeatherData = await weatherJSON.json()
+    const weatherIcon = jsonWeatherData.weather[0].icon
+    const kelvinTemperature = jsonWeatherData.main.temp
+    const celsuisTemperature = kelvinTemperature - 273.15
+    const celsuisTemperatureRounded = Math.round(celsuisTemperature)
 
     // output
-    document.getElementById("cat-fact").innerHTML = "<p>" + randomFactNumber + "</p>"
+    document.getElementById("weather").innerHTML = "<p> The current temperature is " + celsuisTemperatureRounded + " °C.</p>"
+    // document.getElementById("image").innerHTML = "<div> weatherIcon <div/>"
   } catch (error) {
-    console.error(error)
+    // <p> The weather information source is not working at this moment. </p>
   }
 }
