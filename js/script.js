8b8cf9b96b40c00b9393272c6b4d0f4e1bb66cd0
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-22 17:20:26 +0000

    added link

--- js/script.js
+++ js/script.js
@@ -7,19 +7,22 @@
 "use strict"
 
 async function getWeather() {
-
   try {
-    const weatherJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+    const weatherJSON = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+    )
     const jsonWeatherData = await weatherJSON.json()
-    const weatherIcon = jsonWeatherData.weather[0].icon
+    const icon = jsonWeatherData.weather[0].icon
     const kelvinTemperature = jsonWeatherData.main.temp
     const celsuisTemperature = kelvinTemperature - 273.15
     const celsuisTemperatureRounded = Math.round(celsuisTemperature)
 
     // output
-    document.getElementById("weather").innerHTML = "<p> The current temperature is " + celsuisTemperatureRounded + " °C.</p>"
-    document.getElementById("image").innerHTML = "<div> weatherIcon <div/>"
-  } catch (error) {
-    <p> The weather information source is not working at this moment. </p>
+    document.getElementById("weather").innerHTML =
+      "<p> The current temperature is " + celsuisTemperatureRounded + " °C.</p>"
+    document.getElementById("weather-image").innerHTML = "<img src = https://openweathermap.org/img/wn/10d@2x.png"
+  }
+  catch (error) {
+    "<p> The weather information source is not working at this moment. </p>"
   }
 }
